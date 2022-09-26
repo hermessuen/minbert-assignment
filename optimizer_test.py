d844005a55aa647f00f6cd0f9e04efa5a9716b2c
hermessuen <hermessuen@gmail.com>
2022-09-25 23:39:40 -0400

    Update optimizer_test.py

--- optimizer_test.py
+++ optimizer_test.py
@@ -28,5 +28,7 @@ def test_optimizer(opt_class) -> torch.Tensor:
 
 ref = torch.tensor(np.load("optimizer_test.npy"))
 actual = test_optimizer(AdamW)
+print(ref)
+print(actual)
 assert torch.allclose(ref, actual)
 print("Optimizer test passed!")
